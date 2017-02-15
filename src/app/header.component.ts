dc3fa7ca9c8516289535f049858c550f20525988
DAI ANDO <daifloss@gmail.com>
2017-02-16 00:56:29 +0900

    dynamic css change

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public loginmodalcss = "makeithidden";
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onChangeLoginModalCss(logincss: string) {
+  	this.loginmodalcss = logincss;
   }
 
 }
