4c8c7a7fb157ce9f0f65fe6afcd2681da7ca1382
DAI ANDO <daifloss@gmail.com>
2017-02-17 01:51:24 +0900

    add service list

---
+++ src/app/service-list/service-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'svr-service-list',
+  templateUrl: './service-list.component.html',
+  styleUrls: ['./service-list.component.css']
+})
+export class ServiceListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
